e3e0746903b73f8453c4619a4a2230022f4e8a23
Juan Arrivillaga <juanpa.arrivillaga@gmail.com>
2015-10-02 14:32:47 -0400

    EngiVill import script - have to decide how to deal with small floats!

---
+++ importEngineeringVillage.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  2 12:14:48 2015
+
+@author: juan
+"""
+import pandas as pd
+import os
+
+# get the files
+path = '/home/juan/workspace/Battelle/EngineeringVillage'
+
+files = os.listdir(path)
+
+dat = pd.DataFrame()
+for file in files:
+    frame = pd.read_excel(os.path.join(path,file))
+    frame.columns = frame.columns.str.lower()
+    frame.columns = frame.columns.str.replace("[^A-Za-z0-9]","_")
+    dat = dat.append(frame, ignore_index = True)
+dat.to_csv('EngineeringVillage.csv', index_label = False)
